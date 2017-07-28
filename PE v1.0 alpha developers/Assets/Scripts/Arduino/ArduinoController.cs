21b00131b925b0e77833395c0ddfa52041ac3df1
TheKing007 <samuel.salazar@besoft.com.mx>
2017-07-28 00:39:58 -0500

    Upload Directory Project

---
+++ PE v1.0 alpha developers/Assets/Scripts/Arduino/ArduinoController.cs
@@ -0,0 +1,22 @@
+﻿/* Creator and developer: Samuel Salazar Ramírez  samuel.salazar@besoft.com.mx
+ * Company: BeSoftCo
+ * Oficial Site: https://www.besoft.com.mx/
+ */
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArduinoController : ArduinoBaseController {
+
+    public override void Setup()
+    {
+        //On this section you can use pinmode
+        DigitalPinMode(53, Pinmodes.Input);
+        base.Setup();
+    }
+
+    void Update()
+    {
+        //TODO the same thing in loop() function from Arduino IDE
+    }
+}
---
+++ PE v1.0 alpha developers/Assets/Scripts/Arduino/ArduinoPort.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArduinoPort {
+
+    public string PortName { get; set; }
+    public int PortValue { get; set; }
+
+    public ArduinoPort()
+    {
+        this.PortName = "";
+        this.PortValue = 0;
+    }
+
+    public ArduinoPort(string pinName)
+    {
+        this.PortName = pinName;
+        this.PortValue = 0;
+    }
+
+    public ArduinoPort(string portName,int portValue)
+    {
+        this.PortName = portName;
+        this.PortValue = portValue;
+    }
+}
